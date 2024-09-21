de2905e8c628e2007435d5e9a371e30775aada4d
Jonas Given <resaichlabs@gmail.com>
2024-09-21 21:43:46 +0200

    requirements added

--- template.py
+++ template.py
@@ -31,7 +31,8 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "jnfjdjdng.py"
     
 ]
                 
