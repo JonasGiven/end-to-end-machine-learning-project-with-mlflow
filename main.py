9d2d47fd9ed6702578913e05737a657c5792fcf9
Jonas Given <resaichlabs@gmail.com>
2024-09-22 16:46:02 +0200

    exception, logger & utils added

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.mlproject.utils._init_ import logger  # or import logger
+
+
+logger.info("Testing logger")
--- template.py
+++ template.py
@@ -10,18 +10,19 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = "mlproject"
 
 #creating all necessary folders/folder structure and files needed
+
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/_init_.py",
-    f"src/{project_name}/components/_init_.py",
-    f"src/{project_name}/utils/_init_.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/config/_init_.py",
+    f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/_init_.py",
-    f"src/{project_name}/entity/_init_.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
-    f"src/{project_name}/constants/_init_.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
@@ -32,10 +33,11 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html",
-    "jnfjdjdng.py"
-    
+    "test.py"
+
+
 ]
-                
+          
 
 for filepath in list_of_files:
     filepath = Path(filepath)
