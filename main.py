0931577db7b0b125ec75549fd1bda319b0655bc2
Jonas Given <resaichlabs@gmail.com>
2024-10-26 20:42:16 +0200

    Data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlproject.utils._init_ import logger  # or import logger
 from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -7,6 +8,17 @@ try:
     obj = DataIngestionTrainingPipeline()                                #initialising this class
     obj.main()                                                           #calling this main
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
