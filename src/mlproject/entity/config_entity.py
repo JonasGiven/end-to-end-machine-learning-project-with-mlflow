40296ba20e182e72622a1ebd4ecf1bff5aa6ff51
Jonas Given <resaichlabs@gmail.com>
2024-11-01 18:47:01 +0200

    Data evaluation and mlflow added

--- src/mlproject/entity/config_entity.py
+++ src/mlproject/entity/config_entity.py
@@ -30,3 +30,14 @@ class ModelTrainerConfig:
     alpha: float                   #adjusted under params.yaml
     l1_ratio: float                #parameter
     target_column: str             #from schema.yaml which is quality of wine
+    
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    test_data_path: Path
+    model_path: Path
+    all_params: dict
+    metric_file_name: Path
+    target_column: str
+    mlflow_uri: str
